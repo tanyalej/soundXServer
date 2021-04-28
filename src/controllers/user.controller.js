f02b383b7edc4793cd8f6b17841770c9f91efee9
Tania <tanyalej@gmail.com>
2021-04-27 22:13:54 -0500

    Added new user route and endPoint in order to get user basic data such as name and email

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -46,4 +46,14 @@ module.exports = {
       res.status(400).json({ message: error.message })
     }
   },
+  async userData(req, res) {
+    try {
+      const { user } = req
+      const selectedUser = await User.findById(user)
+      console.log('name: ', selectedUser.name, 'email: ', selectedUser.email)
+      res.status(200).json({name:selectedUser.name, email:selectedUser.email })
+    } catch(error) {
+      res.status(400).json({ error })
+    }
+  }
 }
